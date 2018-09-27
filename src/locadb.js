9213d48ac1f2adb49077a6c7cdd16fa9f356ba92
renjithspace <renjithrajeevvk@gmail.com>
2018-09-27 15:25:09 +0530

    Document

--- src/locadb.js
+++ src/locadb.js
@@ -1,29 +1,61 @@
 export default {
+  /**
+   * Store data
+   * @param {String} key 
+   * @param {Any} value 
+   * @returns {undefined} nothing
+   */
   store(key, value) {
     localStorage.setItem(key, value)
   },
 
+  /**
+   * Collect data
+   * @param {String} key 
+   * @returns {Any} value
+   */
   collect(key) {
     return localStorage.getItem(key)
   },
 
+  /**
+   * Remove a specific data
+   * @param {String} key 
+   * @returns {undefined} nothing
+   */
   remove(key) {
     localStorage.removeItem(key)
   },
 
+  /**
+   * Clear all data
+   * @returns {undefined} nothing
+   */
   clear() {
     localStorage.clear()
   },
 
+  /**
+   * Collect keys
+   * @returns {Array} keys
+   */
   keys() {
     return Object.keys(localStorage)
   },
 
+  /**
+   * Data keys size
+   * @returns {number} size
+   */
   size() {
     const keys = this.keys()
     return keys.length
   },
 
+  /**
+   * Collect values
+   * @returns {Array} values
+   */
   values() {
     const keys = this.keys()
     return keys.map(key => localStorage.getItem(key))
