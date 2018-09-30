3215439d51073a2b94dbe2830d71ad9e8e064f4b
renjithspace <renjithrajeevvk@gmail.com>
2018-09-30 19:02:01 +0530

    Merge main and test configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,52 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/locadb.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'locadb.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = [
+  {
+    entry: './src/locadb.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'locadb.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ]
+      ]
+    }
+  },
+  {
+    entry: './test/index.js',
+    output: {
+      path: path.resolve(__dirname, 'test'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'test'),
+      port: 9000,
+      compress: true,
+      hot: true
+    }
   }
-}
+]
