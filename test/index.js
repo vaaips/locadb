3a360e828419e1be0ca2311b680c97b7d6b3477d
renjithspace <renjithrajeevvk@gmail.com>
2018-10-03 18:37:45 +0530

    Add more data type test in get

--- test/index.js
+++ test/index.js
@@ -10,6 +10,36 @@ describe('get', () => {
   it('should return value', () => {
     chai.assert.equal(locadb.get('key'), 'value')
   })
+
+  it('should return data in string if the value was set in string', () => {
+    locadb.set('username', 'johndeo')
+    let value = locadb.get('username')
+    chai.assert.equal(typeof value, 'string')
+  })
+
+  it('should return data in number if the value was set in number', () => {
+    locadb.set('age', 25)
+    let value = locadb.get('age')
+    chai.assert.equal(typeof value, 'number')
+  })
+
+  it('should return data in boolean if the value was set in boolean', () => {
+    locadb.set('can', true)
+    let value = locadb.get('can')
+    chai.assert.equal(typeof value, 'boolean')
+  })
+
+  it('should return data in array if the value was set in array', () => {
+    locadb.set('numbers', [1, 2, 3, 4])
+    let value = locadb.get('numbers')
+    chai.assert.equal(value.length, 4)
+  })
+
+  it('should return data in object if the value was set in object', () => {
+    locadb.set('user', { name: 'John Deo', age: 25 })
+    let value = locadb.get('user')
+    chai.assert.equal(value.age, 25)
+  })
 })
 
 describe('remove', () => {
